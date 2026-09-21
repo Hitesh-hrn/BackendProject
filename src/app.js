25924ad7ccbd9cdfbe01e7edbdc3adeff6d39aed
Neelesh Kumar <tumharaemail@domain.com>
2026-09-22 02:26:27 +0530

    add register controller and bug fixes

--- src/app.js
+++ src/app.js
@@ -12,6 +12,7 @@ app.use(cors({
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
+app.use(cookieParser())
 
 //routes import
 
@@ -19,6 +20,7 @@ import userRouter from './routes/user.routes.js';
 
 //routes declaration 
 app.use("/api/v1/users", userRouter)
+
 
 //http://localhost:8000/api/v1/users/register/
 
